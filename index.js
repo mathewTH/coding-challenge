6014ca15cad61790738452453a849c5a2ba5fdfc
Mathew Tonkin Henwood <7479946+mathewTH@users.noreply.github.com>
2020-11-09 23:07:01 +1300

    Add Gross Profit Margin calculation

--- index.js
+++ index.js
@@ -2,12 +2,21 @@
 
 const fs = require('fs');
 const data = JSON.parse(fs.readFileSync('data.json'));
-const numberFormat = new Intl.NumberFormat('en-NZ', {
+
+const currencyFormat = new Intl.NumberFormat('en-NZ', {
   maximumFractionDigits: 0,
 });
+const revenue = calcRevenue(data.data);
+console.log('Revenue: $%s', currencyFormat.format(revenue));
+console.log('Expenses: $%s', currencyFormat.format(calcExpenses(data.data)));
 
-console.log('Revenue: $%s', numberFormat.format(calcRevenue(data.data)));
-console.log('Expenses: $%s', numberFormat.format(calcExpenses(data.data)));
+const percentFormat = new Intl.NumberFormat('en-NZ', {
+  maximumFractionDigits: 1,
+});
+console.log(
+  'Gross Profit Margin: %s%%',
+  percentFormat.format(100*calcGrossProfitMargin(data.data, revenue))
+);
 
 /**
  * filter out revenue entries and add total_value to calculate revenue
@@ -29,4 +38,19 @@ function calcExpenses(dataEntries) {
   return dataEntries
     .filter(entry => entry.account_category === 'expense')
     .reduce((accumulator, entry) => entry.total_value + accumulator, 0);
+}
+
+/**
+ * add sales debits and divide by revenue
+ * @param  {Array} dataEntries array of data entries
+ * @param  {Number} revenue     calculated revenue
+ * @return {Number}             calculated gross profit margin
+ */
+function calcGrossProfitMargin(dataEntries, revenue) {
+  const salesDebits = dataEntries
+    .filter(
+      entry => entry.account_type === 'sales' && entry.value_type === 'debit'
+    )
+    .reduce((accumulator, entry) => entry.total_value + accumulator, 0);
+  return salesDebits / revenue;
 }
