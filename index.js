26417dca7c3a2890f6f4b083c7bec797ec68d7c3
Mathew Tonkin Henwood <7479946+mathewTH@users.noreply.github.com>
2020-11-09 22:14:37 +1300

    Implement expenses calculation

--- index.js
+++ index.js
@@ -3,7 +3,9 @@
 const fs = require('fs');
 const data = JSON.parse(fs.readFileSync('data.json'));
 const numberFormat = new Intl.NumberFormat('en-NZ', { maximumFractionDigits: 0 });
+
 console.log('Revenue: $%s', numberFormat.format(calcRevenue(data.data)));
+console.log('Expenses: $%s', numberFormat.format(calcExpenses(data.data)));
 
 /**
  * filter out revenue entries and add total_value to calculate revenue
@@ -13,4 +15,14 @@ console.log('Revenue: $%s', numberFormat.format(calcRevenue(data.data)));
 function calcRevenue(dataEntries) {
   return dataEntries.filter(entry => entry.account_category === 'revenue')
       .reduce((accumulator, entry) => (entry.total_value + accumulator), 0);
+}
+
+/**
+ * filter out expense entries and add total_value to calculate expenses
+ * @param  {Array} dataEntries array of data entries
+ * @return {Number}            calculated expenses
+ */
+function calcExpenses(dataEntries) {
+  return dataEntries.filter(entry => entry.account_category === 'expense')
+      .reduce((accumulator, entry) => (entry.total_value + accumulator), 0)
 }
