05cb89baac6fdb3565399ee3fb2e1c61e49a2e53
Mathew Tonkin Henwood <7479946+mathewTH@users.noreply.github.com>
2020-11-09 22:34:14 +1300

    Add prettier config and format

--- index.js
+++ index.js
@@ -2,19 +2,22 @@
 
 const fs = require('fs');
 const data = JSON.parse(fs.readFileSync('data.json'));
-const numberFormat = new Intl.NumberFormat('en-NZ', { maximumFractionDigits: 0 });
+const numberFormat = new Intl.NumberFormat('en-NZ', {
+  maximumFractionDigits: 0,
+});
 
 console.log('Revenue: $%s', numberFormat.format(calcRevenue(data.data)));
 console.log('Expenses: $%s', numberFormat.format(calcExpenses(data.data)));
 
 /**
  * filter out revenue entries and add total_value to calculate revenue
- * @param  {Array} dataEntries   array of data entries
- * @return {number}              calculated revenue
+ * @param  {Array} dataEntries array of data entries
+ * @return {Number}            calculated revenue
  */
 function calcRevenue(dataEntries) {
-  return dataEntries.filter(entry => entry.account_category === 'revenue')
-      .reduce((accumulator, entry) => (entry.total_value + accumulator), 0);
+  return dataEntries
+    .filter(entry => entry.account_category === 'revenue')
+    .reduce((accumulator, entry) => entry.total_value + accumulator, 0);
 }
 
 /**
@@ -23,6 +26,7 @@ function calcRevenue(dataEntries) {
  * @return {Number}            calculated expenses
  */
 function calcExpenses(dataEntries) {
-  return dataEntries.filter(entry => entry.account_category === 'expense')
-      .reduce((accumulator, entry) => (entry.total_value + accumulator), 0)
+  return dataEntries
+    .filter(entry => entry.account_category === 'expense')
+    .reduce((accumulator, entry) => entry.total_value + accumulator, 0);
 }
